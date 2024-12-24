8f7a266eaba7b7ef4a2b7e4b9c23c1177dcc1a62
ELAB206-02\Administrator <daniyalnawaz@iqra.edu.pk>
2024-12-25 04:29:52 +0500

    thymeleaf, ngrok

--- src/main/java/com/sqe/week9/controller/GreetingsController.java
+++ src/main/java/com/sqe/week9/controller/GreetingsController.java
@@ -1,7 +1,10 @@
 package com.sqe.week9.controller;
 
 import com.sqe.week9.service.GreetingService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +19,11 @@ public class GreetingsController {
     @GetMapping("/greeting")
     public String getGreetings(){
         return greetingService.getGreeting();
+    }
+
+    @GetMapping("/test")
+    public String home(Model model) {
+        model.addAttribute("currentDateTime", LocalDateTime.now());
+        return "this is the server deployment"; // Maps to src/main/resources/templates/index.html
     }
 }
